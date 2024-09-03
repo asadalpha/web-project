357a0828a8a60ae5683a497071b70ae48246dc1b
Asad <asadalpha77@gmail.com>
2024-09-03 12:04:55 +0530

    changes by alpha

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const cards = document.querySelectorAll('.popular-card');
+    
+    cards.forEach((card, index) => {
+      setTimeout(() => {
+        card.classList.add('show');
+      }, index * 100); // Staggered delay for animation
+    });
+  });
+  
